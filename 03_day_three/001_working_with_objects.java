3bfd4780c9a10138dc02a32bd975df1f312ccc23
mau5atron <gabrielbetancourt96@outlook.com>
2019-01-24 16:49:01 -0500

    started day three with objects

---
+++ 03_day_three/001_working_with_objects.java
@@ -0,0 +1,20 @@
+class WorkingObjects {
+	public static void main(String[] arguments){
+		/*
+			Java is an object-oriented programming language. When you do work 
+			in Java, you primarily use objects to get the job done. You create 
+			objects, modify them, change their variables, call their methods, 
+			and combine them with other objects. You develop classes, create
+			objects out of those classes, and use them with other classes and
+			objects. 
+
+			Today, you work extensively with objects as you undertake these 
+			essential tasks: 
+
+			- Creating objects 
+			- Testing and modifying their class and instance variables 
+			- Calling an objects methods 
+			- Converting objects from one class to another
+		*/ 
+	}
+}
