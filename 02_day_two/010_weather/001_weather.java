0e156a5c5fedf585c75945dabc2fd161e8066fd7
mau5atron <gabrielbetancourt96@outlook.com>
2019-01-24 12:22:12 -0500

    shrunk spacing

--- 02_day_two/010_weather/001_weather.java
+++ 02_day_two/010_weather/001_weather.java
@@ -2,10 +2,8 @@ class Weather {
 	public static void main(String[] arguments){
 		float fahr = 86;
 		float to_fahr;
-
 		float to_cel;
 		float cel = 32;
-
 		to_cel = ((fahr - 32)/9) * 5;
 		System.out.println("It is currently " + fahr + " degrees fahrenheit."
 		);
@@ -14,11 +12,9 @@ class Weather {
 
 		to_fahr = ((cel * 9)/5) + 32;
 		System.out.println(
-			"It is currently " + cel + " degress celsius outside"
-		);
+			"It is currently " + cel + " degress celsius outside");
 
 		System.out.println(
-			cel + " degress celsius in fahrenheit is: " + to_fahr + "\n"
-		);
+			cel + " degress celsius in fahrenheit is: " + to_fahr + "\n");
 	}
 }
